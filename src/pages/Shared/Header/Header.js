ccd92f7249f7f0626a81eedbc5590589a1267bce
TechDev-CEO <shoriful521399@gmail.com>
2022-04-17 20:37:18 +0600

    Navbar Complete

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './Header.css';
+import logo from '../../../images/logo.png'
 
 const Header = () => {
     return (
@@ -8,23 +9,26 @@ const Header = () => {
             <div className="top-header">
                 <p className='text-center'>Discount up to 50% only this month for member medipharma.</p>
             </div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" bg="transparent" variant="dark">
                 <Container>
-                <Navbar.Brand href="#home" className='me-5'>React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home" className='me-5'><img src={logo} width={200} alt="" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-center'>
                     <Nav className="me-auto">
                     </Nav>
                     <Nav>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                    <Nav.Link href="#features">Home</Nav.Link>
+                    <Nav.Link href="#pricing">About Us</Nav.Link>
+                    <NavDropdown title="Category" id="collasible-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
+                    <Nav.Link href="#pricing">Contact us</Nav.Link>
+                    <Nav.Link href="#pricing">Service</Nav.Link>
+                    
                     <NavDropdown title="Login/Register" id="collasible-nav-dropdown" className='ms-5'>
                         <NavDropdown.Item href="#action/3.1">About</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
